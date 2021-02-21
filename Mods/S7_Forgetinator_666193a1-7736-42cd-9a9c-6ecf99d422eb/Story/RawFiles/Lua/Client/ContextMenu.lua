4b5b7a12dfa327f34973a70bc5d4757eef46d2bb
Hunter-Ghost-27 <Shresht7@users.noreply.github.com>
2021-02-21 15:53:08 +0530

    CtxMenu registration after session-load

--- Mods/S7_Forgetinator_666193a1-7736-42cd-9a9c-6ecf99d422eb/Story/RawFiles/Lua/Client/ContextMenu.lua
+++ Mods/S7_Forgetinator_666193a1-7736-42cd-9a9c-6ecf99d422eb/Story/RawFiles/Lua/Client/ContextMenu.lua
@@ -1,26 +1,28 @@
-UCL = Mods['S7_UI_Components_Library'] -- Import UI Components Library
 
 --  ============
 --  CONTEXT MENU
 --  ============
 
-UCL.ContextMenu:Register({
-    ["RootTemplate::" .. ForgetinatorTemplate] = {
-        {
-            ['actionID'] = 27301,
-            ['clickSound'] = true,
-            ['text'] = Color:Red("Turn Safeties OFF"),
-            ['isDisabled'] = false,
-            ['isLegal'] = true
+Ext.RegisterListener('SessionLoaded', function ()
+    UCL = Mods['S7_UI_Components_Library'] -- Import UI Components Library
+    UCL.ContextMenu:Register({
+        ["RootTemplate::" .. ForgetinatorTemplate] = {
+            {
+                ['actionID'] = 27301,
+                ['clickSound'] = true,
+                ['text'] = Color:Red("Turn Safeties OFF"),
+                ['isDisabled'] = false,
+                ['isLegal'] = true
+            }
+        },
+        ["RootTemplate::" .. ForgetinatorSafetyOffTemplate] = {
+            {
+                ['actionID'] = 27302,
+                ['clickSound'] = true,
+                ['text'] = Color:Green("Turn Safeties ON"),
+                ['isDisabled'] = false,
+                ['isLegal'] = true
+            }
         }
-    },
-    ["RootTemplate::" .. ForgetinatorSafetyOffTemplate] = {
-        {
-            ['actionID'] = 27302,
-            ['clickSound'] = true,
-            ['text'] = Color:Green("Turn Safeties ON"),
-            ['isDisabled'] = false,
-            ['isLegal'] = true
-        }
-    }
-})
+    })
+end)
